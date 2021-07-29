9c8f02b3c27e24d8e22de6e24126cd346ee1b950
izzaz <izzazahmed134@gmail.com>
2021-07-29 04:36:07 -0700

    added fuse fn

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express");
 var cors = require('cors')
 const Fuse = require("fuse.js");
 const app = express();
+app.use(express.json());
 app.use(cors())
 const port = 3000;
 
 const myArray = ["ayan", "assam", "banana", "hatigaon", "hati", "sati"];
 
 const options = {
+  threshold: 0.6,
   keys: ["title"],
 };
 
@@ -26,10 +28,13 @@ app.get("/post", (req, res) => {
 // SEARCH FUNCTION
 
 app.post("/search", (req, res) => {
-  const pattern = req.body;
+  const pattern = req.body.query;
+  console.log(pattern)
   const x = fuse.search(pattern);
-  console.log(x);
-  res.send(x);
+
+  console.log(x)
+  res.json(!!x ? x : []);
+
 });
 
 app.listen(port, () => {
