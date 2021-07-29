be0121e954fe0e0d278dc0a69e7d01f231017af5
izzaz <izzazahmed134@gmail.com>
2021-07-29 00:58:15 -0700

    test post route

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/post', (req, res) => {
+  res.send('Hello Im am post')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
